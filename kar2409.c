abf2d86c58568181ca475eca4bb3e6d3a702d4b2
karyne307 <heloyza.amorimsilva@gmail.com>
2024-09-24 11:15:33 -0300

    Add files via upload

---
+++ kar2409.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int numero1 = 6;
+    int numero2 = 20;
+    int soma;
+    soma = numero1 + numero2
+    printf("%d",soma);
+    char letra= '1'
+    char nome [4]={"helo"};
+    printf("%c",letra);
+    printf("%s",nome);
+
+}
